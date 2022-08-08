ebeb8fd48bcaf4818cf5ebb3d81ac8e6010016fe
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-08 12:41:49 -0400

    Update and rename CopynRename.py to Copy.py

--- Copy.py
+++ Copy.py
@@ -1,4 +1,4 @@
-import os, datetime
+import os, time
 import shutil
 
 print('Enter the keyword of the files you want to copy: ', end = '')
@@ -13,5 +13,4 @@ dst_folder = input()
 for path, subdirs, files in os.walk(source_folder):
     for name in files:
         if(name.startswith(keyword) & name.endswith(".pptx")):
-            filetime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(path, name)))
             new_location = shutil.copy(os.path.join(path, name), dst_folder)
