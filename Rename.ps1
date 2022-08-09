472ac630c022c5be068f19d74dd7449644eb20ff
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-09 10:46:23 -0400

    Add files via upload

--- Rename.ps1
+++ Rename.ps1
@@ -1,5 +1,5 @@
-$keyword = Read-Host -Prompt "Enter your keyword"
-$location = Read-Host -Prompt "Enter your location"
+$keyword = Read-Host -Prompt "Enter the search keyword"
+$location = Read-Host -Prompt "Enter the search location"
 Set-Location -Path "$location"
 Get-ChildItem | Rename-Item -NewName {$keyword + " " + $_.LastWriteTime.ToString("yyyy-MM-dd HH.mm") + ($_.Extension)}
 Write-Host "Done!" -ForegroundColor Green
