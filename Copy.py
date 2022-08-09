fe42df72a3f7fffefe08a4a62394d508c230ce2f
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-09 10:45:12 -0400

    Add files via upload

--- Copy.py
+++ Copy.py
@@ -1,18 +1,19 @@
 import os
 import shutil
 
-print('Enter the keyword of the files you want to copy: ', end = '')
+print('Enter the search keyword: ', end = '')
 keyword = input()
 
-print('Enter the general location of where the files is store: ', end = '')
+print('Enter the general location of where the files are stored: ', end = '')
 source_folder = input()
 
-print('Enter a location to where you would like to store you files: ', end = '')
+print('Enter a location to where you save the output file: ', end = '')
 dst_folder = input()
 
 for path, subdirs, files in os.walk(source_folder):
     for name in files:
         if(name.startswith(keyword) & name.endswith(".pptx")):
+
             new_location = shutil.copy(os.path.join(path, name), dst_folder)
-            
+
 print('Done!')
