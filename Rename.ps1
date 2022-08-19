bc347ab279c03c49d264e4ab75f906be374df204
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-19 02:06:15 -0500

    Update Rename.ps1

--- Rename.ps1
+++ Rename.ps1
@@ -1,6 +1,10 @@
 $keyword = Read-Host -Prompt "Enter the search keyword"
 $location = Read-Host -Prompt "Enter the search location"
 Set-Location -Path "$location"
-Get-ChildItem | Rename-Item -NewName {$keyword + " " + $_.LastWriteTime.ToString("yyyy-MM-dd HH.mm") + ($_.Extension)}
+Get-ChildItem *.pptx |ForEach-Object {
+    $NewName = $keyword + " " + $_.LastWriteTime.toString("yyyy-MM-dd-HH-mm")+($script:i++) + ".pptx"
+    $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+    Move-Item -Path $_.FullName -Destination $Destination -Force
+}
 Write-Host "Done!" -ForegroundColor Green
 Read-Host -Prompt "Press Enter to exit"
