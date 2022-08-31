3f184ae9d3d0cfff3c2991b18458586d5b6bc8d3
Richie Zhou <65792694+arunike@users.noreply.github.com>
2022-08-31 10:55:01 -0500

    Update Copy.py

--- Copy.py
+++ Copy.py
@@ -1,19 +1,27 @@
+## Imports
 import os
 import shutil
 
-print('Enter the search keyword: ', end = '')
-keyword = input()
+## Copy Function
+def Copy(): 
 
-print('Enter the general location of where the files are stored: ', end = '')
-source_folder = input()
+    ## User Input
+    print('Enter the search keyword: ', end = '')
+    keyword = input()
 
-print('Enter a location to where you save the output file: ', end = '')
-dst_folder = input()
+    print('Enter the general location of where the files are stored: ', end = '')
+    source_folder = input()
 
-for path, subdirs, files in os.walk(source_folder):
-    for name in files:
-        if(name.startswith(keyword) & name.endswith(".pptx")):
+    print('Enter a location to where you save the output file: ', end = '')
+    dst_folder = input()
 
-            new_location = shutil.copy(os.path.join(path, name), dst_folder)
+    ## Search for files
+    for path, files in os.walk(source_folder):
+        for name in files:
+            if(name.startswith(keyword) & name.endswith(".pptx")):
 
-print('Done!')
+                shutil.copy(os.path.join(path, name), dst_folder) ## Copy file to destination folder
+
+    print('Done!')
+
+Copy()
